4825909b9b0217c273d073af3a08c18416d1afd2
Charlie Brooking <cbrookin@thoughtworks.com>
2016-01-15 01:04:18 +1000

    convert let exprs to functions

--- shokunin.hs
+++ shokunin.hs
@@ -1,15 +1,16 @@
 import Data.Char
 import Data.List
 
+topLeft maxLetter =
+  zipWith3 (\x y z -> x ++ [y] ++ z) (reverse spaces) letters spaces
+  where letters = ['A'..toUpper maxLetter]
+        spaces = [replicate n ' ' | n <- [0..length letters]]
+mirrorDown top = top ++ (tail $ reverse top)
+mirrorRight left = [x ++ tail (reverse x) | x <- left]
+diamond maxLetter = mirrorRight $ mirrorDown $ topLeft maxLetter
+
 main = do
-  widestLetter <- getChar
-  if elem widestLetter (['a'..'z'] ++ ['A'..'Z'])
-    then
-      let letters = ['A'..toUpper widestLetter]
-          spaces = [replicate n ' ' | n <- [0..length letters]]
-          topLeft = zipWith3 (\x y z -> x ++ [y] ++ z) (reverse spaces) letters spaces
-          left = topLeft ++ (tail $ reverse topLeft)
-          diamond = intersperse "" [x ++ tail (reverse x) | x <- left]
-      in mapM_ putStrLn diamond
-    else
-      putStrLn "INVALID INPUT"
+  maxLetter <- getChar
+  if elem maxLetter (['a'..'z'] ++ ['A'..'Z'])
+    then mapM_ putStrLn $ intersperse "" (diamond maxLetter)
+    else putStrLn "INVALID INPUT"
