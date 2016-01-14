6aae78047230fc1d21830df0f1c963b7f43b9859
Charlie Brooking <cbrookin@thoughtworks.com>
2016-01-15 01:24:07 +1000

    use generic mirror function

--- shokunin.hs
+++ shokunin.hs
@@ -1,14 +1,14 @@
 import Data.Char
 import Data.List
 
-topLeft maxLetter =
-  zipWith3 (\x y z -> x ++ [y] ++ z) (reverse spaces) letters spaces
+topLeft maxLetter = zipWith3 (\x y z -> x ++ [y] ++ z) (reverse spaces) letters spaces
   where letters = ['A'..toUpper maxLetter]
         spaces = [replicate n ' ' | n <- [0..length letters]]
 
-mirrorDown top = top ++ tail (reverse top)
-mirrorRight left = [x ++ tail (reverse x) | x <- left]
 diamond maxLetter = mirrorRight $ mirrorDown $ topLeft maxLetter
+  where mirror xs = xs ++ tail (reverse xs)
+        mirrorDown = mirror
+        mirrorRight = map mirror
 
 main = do
   maxLetter <- getChar
