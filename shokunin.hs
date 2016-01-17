8467a7eff5d72b4515628647e72534e8d294ba99
Matt Newman <mdjnewman@gmail.com>
2016-01-17 20:04:45 +1000

    Uppercase straight away to remove extra range check

--- shokunin.hs
+++ shokunin.hs
@@ -3,7 +3,7 @@ import Data.List
 import System.Environment
 
 topLeft maxLetter = zipWith3 (\x y z -> x ++ [y] ++ z) (reverse spaces) letters spaces
-  where letters = ['A'..toUpper maxLetter]
+  where letters = ['A'..maxLetter]
         spaces = [replicate n ' ' | n <- [0..length letters]]
 
 diamond = mirrorRight . mirrorDown . topLeft
@@ -16,7 +16,7 @@ errorMessage = "INVALID INPUT"
 main = do
   args <- getArgs
   case args of
-    [[c]] -> if c `elem` (['a'..'z'] ++ ['A'..'Z'])
+    [[c]] -> if toUpper c `elem` ['A'..'Z']
                 then putStrLn $ intercalate "\n\n" (diamond c)
                 else putStrLn errorMessage
     _     ->         putStrLn errorMessage
